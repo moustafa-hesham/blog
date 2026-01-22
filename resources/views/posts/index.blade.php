326da34c974833f7be5f21544f673d5507bf6091
moustafa-hesham <moustafa.hesham3@gmail.com>
2026-01-22 13:46:31 +0200

    adding layout (temple inheritance)

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,37 +1,9 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.app')
+@section('title')
+    Index
+@endsection
+@section('content')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Index</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-</head>
-
-
-
-</html>
-
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <a class="navbar-brand fw-bold" href="#">Mitch Blog Post</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">All Posts</a>
-                </li>
-
-            </ul>
-        </div>
-    </div>
-</nav>
-
-<div class="container">
 
     <div class="text-center my-3">
 
@@ -71,4 +43,4 @@
 
         </tbody>
     </table>
-</div>
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,36 +1,10 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Show Post</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-</head>
+@extends('layouts.app')
+@section('title')
+    Show
+@endsection
+@section('content')
 
 
-
-</html>
-
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <a class="navbar-brand fw-bold" href="#">Mitch Blog Post</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('posts.show') }}">All Posts</a>
-                </li>
-
-            </ul>
-        </div>
-    </div>
-</nav>
-<div class="container">
     <div class="card mt-4">
         <div class="card-header">
             Posts Info
@@ -52,6 +26,4 @@
 
         </div>
     </div>
-</div>
-
-</div>
+@endsection
